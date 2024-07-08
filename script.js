a7d0100e9969d1fef96f766d49fba202ad02f97e
SalxIeva <ieva.saldukaite@gmail.com>
2024-07-08 15:23:33 +0100

    pseudo code added for createGrid function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hello");
+
+// PSEUDO CODE
+
+// Create a div container to store grid div's 
+// Create a function to create grid
+// Clear previous grid
+// Add flexbox to make divs appear as grid
+// Calculate the size of each grid
+// Inside the function loop size * size to create a grid
+// Add a hover event listener to the grid div.
+// Append each grid div to the container
+// Add hover effect to each grid div to change color when hoovered over it
+
